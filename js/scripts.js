82ca216ca3fe905ba22d7a1dab35c1d2dd758abf
Alvaroescobar97 <alvaro325@hotmail.com>
2021-02-24 12:26:13 -0500

    adding js folder, styling the contact form and deleting material desing libraries

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+(function(){
+
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var btns = document.querySelectorAll('.nav-link');
+
+        for (var i = 0; i < btns.length; i++) {
+
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+
+                if (current.length > 0) {
+                    current[0].className = current[0].className.replace(" active", "");
+                }
+
+                this.className += " active";
+            });
+
+        }
+        
+    });
+
+})();
